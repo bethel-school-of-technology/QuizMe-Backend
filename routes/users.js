0504243acca2792bacd87aff0a932467b63a4f17
Steve Thomas <stthomas4531@gmail.com>
2020-06-06 12:12:22 -0600

    testing commit users route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET all users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET one user listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
