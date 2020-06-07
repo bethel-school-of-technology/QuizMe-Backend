d7c1eb217ccf6d77d6d9777ae07a045a58d6ec6a
Steve Thomas <stthomas4531@gmail.com>
2020-06-06 21:30:43 -0600

    Users Get V1

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let cors = require('cors');
 var logger = require('morgan');
 var models = require('./models');
 
@@ -8,7 +10,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(bodyParser.json());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,24 @@ var router = express.Router();
 
 /* GET all users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.users.findAll().then(users =>{
+  res.json(users)
+  })
 });
 
 /* GET one user listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users:id', function(req, res, next) {
+  models.users.findByPk(parseInt(req.params.id)).then(user =>{
+    res.json(user)
+  })
+});
+
+/* Create user */
+router.post('/user', function(req, res, next) {
+  models.post.create(req.body).then(() => {
+  res.json({message: 'Created user!'})
+    
+  });
 });
 
 module.exports = router;
