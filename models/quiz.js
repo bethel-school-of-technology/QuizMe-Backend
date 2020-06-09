38353beef1343042067a0e32caf0e67443930451
Alycia Crane <alyciacrane@Alycias-MacBook-Pro.local>
2020-06-09 18:47:43 -0500

    added category field to quiz model

--- models/quiz.js
+++ models/quiz.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
     },
     name: {
       type: DataTypes.STRING
+    },
+    category: {
+      type: DataTypes.STRING
     }
   }, {});
   quiz.associate = function(models) {
