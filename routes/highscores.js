dc31690fa7978bc3df7931a6adc375487c2dddb1
Michael Alexander <michaela171@gmail.com>
2020-06-16 20:58:07 -0500

    started adding suecurity to PUT highscores/:id route, still testing

--- routes/highscores.js
+++ routes/highscores.js
@@ -2,16 +2,33 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql2');
 var models = require('../models');
+var authService = require('../services/auth');
 
-router.put("/highscores/:id", authorization.needed('ADMIN'), function (req, res, next) {
-    let highscoreId = parseInt(req.params.id);
-    models.highscores
-      .update(req.body, { where: { highscore_id: highscoreId } })
-      .then(result => res.redirect('/highscores/' + highscoreId))
-      .catch(err => {
-        res.status(400);
-        res.send("There was a problem updating the highscore.");
-      });
+router.put("/:id", function (req, res, next) {
+  let token = req.cookies.jwt;
+  let highscoreId = parseInt(req.params.id);
+  if (token) {
+    authService.verifyUser(token)
+      .then(models.highscores
+    .update(req.body, { where: { highscore_id: highscoreId } })
+    .then(result => res.redirect('/highscores/' + highscoreId))
+    .catch(err => {
+      res.status(400);
+      res.send("There was a problem updating the highscore.");
+    }));
+  } else {
+    res.status(401);
+    res.send('Must be logged in as an Administrator.');
+  };
+
+  // let highscoreId = parseInt(req.params.id);
+  // models.highscores
+  //   .update(req.body, { where: { highscore_id: highscoreId } })
+  //   .then(result => res.redirect('/highscores/' + highscoreId))
+  //   .catch(err => {
+  //     res.status(400);
+  //     res.send("There was a problem updating the highscore.");
+  //   });
   });
   
 
