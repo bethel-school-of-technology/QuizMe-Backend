dda3fc3235a43ead38894fa621217cb629583755
SeanMXD <SeanMXD@gmail.com>
2020-06-25 00:04:32 -0400

    Broke some things

--- routes/highscores.js
+++ routes/highscores.js
@@ -19,13 +19,81 @@ router.post('/', function (req, res, next) {
 
 // GET for the highscores page
 router.get('/', function(req, res, next) {
-    models.highscores
-      .findAll()
-      .then(highscoresFound => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(highscoresFound));
-      });
+    // models.highscores
+    //   .findAll()
+    //   .then(highscoresFound => {
+    //     res.setHeader('Content-Type', 'application/json');
+    //     res.JSON(highscoresFound);
+    //   });
+    res.json([
+      {
+        id: 1,
+        category: 15,
+        playername: "SeanMXD",
+        highscore: 1200
+      },
+      {
+        id: 1,
+        category: 15,
+        playername: "SeanMXD",
+        highscore: 1200
+      },
+      {
+        id: 1,
+        category: 15,
+        playername: "SeanMXD",
+        highscore: 1200
+      },
+      {
+        id: 1,
+        category: 16,
+        playername: "SeanMXD",
+        highscore: 1200
+      },
+      {
+        id: 1,
+        category: 16,
+        playername: "SeanMXD",
+        highscore: 1200
+      },
+      {
+        id: 1,
+        category: 16,
+        playername: "SeanMXD",
+        highscore: 1200
+      }
+    ])
   });
+
+  // GET for the highscores page
+router.get('/:id', function(req, res, next) {
+  // models.highscores
+  //   .findAll()
+  //   .then(highscoresFound => {
+  //     res.setHeader('Content-Type', 'application/json');
+  //     res.JSON(highscoresFound);
+  //   });
+  res.json([
+    {
+      id: 1,
+      category: req.params.id,
+      playername: "SeanMXD",
+      highscore: 1200
+    },
+    {
+      id: 1,
+      category: req.params.id,
+      playername: "SeanMXD",
+      highscore: 1200
+    },
+    {
+      id: 1,
+      category: req.params.id,
+      playername: "SeanMXD",
+      highscore: 1200
+    }
+  ])
+});
 
 // secured put route for admins
 router.put("/:id", function (req, res, next) {
