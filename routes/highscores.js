8a4166a910e2140e194c30ca022b556497068211
Steve Thomas <stthomas4531@gmail.com>
2020-06-25 19:21:44 -0600

    fixed routes

--- routes/highscores.js
+++ routes/highscores.js
@@ -18,81 +18,21 @@ router.post('/', function (req, res, next) {
   });
 
 // GET for the highscores page
-router.get('/', function(req, res, next) {
-    // models.highscores
-    //   .findAll()
-    //   .then(highscoresFound => {
-    //     res.setHeader('Content-Type', 'application/json');
-    //     res.JSON(highscoresFound);
-    //   });
-    res.json([
-      {
-        id: 1,
-        category: 15,
-        playername: "SeanMXD",
-        highscore: 1200
-      },
-      {
-        id: 1,
-        category: 15,
-        playername: "SeanMXD",
-        highscore: 1200
-      },
-      {
-        id: 1,
-        category: 15,
-        playername: "SeanMXD",
-        highscore: 1200
-      },
-      {
-        id: 1,
-        category: 16,
-        playername: "SeanMXD",
-        highscore: 1200
-      },
-      {
-        id: 1,
-        category: 16,
-        playername: "SeanMXD",
-        highscore: 1200
-      },
-      {
-        id: 1,
-        category: 16,
-        playername: "SeanMXD",
-        highscore: 1200
-      }
-    ])
+router.get('/undefined', function(req, res, next) {
+    models.highscores
+      .findAll()
+      .then(highscoresFound => {
+        res.json(highscoresFound);
+      });
   });
 
   // GET for the highscores page
 router.get('/:id', function(req, res, next) {
-  // models.highscores
-  //   .findAll()
-  //   .then(highscoresFound => {
-  //     res.setHeader('Content-Type', 'application/json');
-  //     res.JSON(highscoresFound);
-  //   });
-  res.json([
-    {
-      id: 1,
-      category: req.params.id,
-      playername: "SeanMXD",
-      highscore: 1200
-    },
-    {
-      id: 1,
-      category: req.params.id,
-      playername: "SeanMXD",
-      highscore: 1200
-    },
-    {
-      id: 1,
-      category: req.params.id,
-      playername: "SeanMXD",
-      highscore: 1200
-    }
-  ])
+  models.highscores
+    .findAll({where: {category: parseInt(req.params.id)}})
+    .then(highscoresFound => {
+      res.json(highscoresFound);
+    });
 });
 
 // secured put route for admins
