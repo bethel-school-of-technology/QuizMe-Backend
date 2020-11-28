730cc95a66ad4b6e5e780ee7e8dcdce879aa57a5
Sean Combs <seanmxd@gmail.com>
2020-11-27 20:24:14 -0500

    Allow any host from now on

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(function(req, res, next) {
     next();
 });
 app.use(bodyParser.json());
-app.use(cors({origin: "http://localhost:3000"}));
+app.use(cors({origin: "*"}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
